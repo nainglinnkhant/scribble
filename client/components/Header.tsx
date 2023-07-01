1757c50764f6c5cc832b31e780af8632cb748458
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-07-02 03:06:31 +0630

    feat(ui): improve header ui

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -8,11 +8,11 @@ import RightPanel from '@/components/RightPanel'
 
 export default function Header() {
   return (
-    <header className='sticky top-0 z-40 w-full select-none border-b'>
+    <header className='sticky top-0 z-40 w-full select-none border-b bg-background/80 saturate-200 backdrop-blur-sm'>
       <div className='container flex items-center justify-between py-3'>
         <h2 className='text-lg font-medium'>Scribble</h2>
 
-        <div className='flex items-center gap-4'>
+        <div className='flex items-center gap-3 md:gap-4'>
           <SaveButton />
 
           <ThemeMenu />
