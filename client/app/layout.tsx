68295ad19ac6e494b4a9f7bccf50372e41be52e7
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-07-06 02:24:59 +0630

    fix: fix 'extra attributes from the server' warning

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -14,8 +14,8 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>
+    <html lang='en' suppressHydrationWarning>
+      <body className={inter.className} suppressHydrationWarning>
         <Providers>{children}</Providers>
 
         <Analytics />
