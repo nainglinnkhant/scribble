9ed4831840dec7805fba3dfc6e0589cd57150e0d
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-07-09 22:58:18 +0630

    feat: add hosted backend url

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -1,5 +1,8 @@
 import { io } from 'socket.io-client'
 
-const SERVER = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3001'
+const SERVER =
+  process.env.NODE_ENV === 'production'
+    ? 'scribble-production-d6c0.up.railway.app'
+    : 'http://localhost:3001'
 
 export const socket = io(SERVER, { transports: ['websocket'] })
