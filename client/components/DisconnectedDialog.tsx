52508c3cd40d1d88905a9ba60b93de8569f1b0b5
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-07-22 18:08:20 +0630

    refactor: remove unnecessary React imports

--- client/components/DisconnectedDialog.tsx
+++ client/components/DisconnectedDialog.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
 import { socket } from '@/lib/socket'
 import {
--- client/components/ThemeMenuButton.tsx
+++ client/components/ThemeMenuButton.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import React from 'react'
 import { useTheme } from 'next-themes'
 import { Laptop, Moon, Sun } from 'lucide-react'
 
