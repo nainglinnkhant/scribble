006dcf6314195af8233317efe0b819161d28d712
contigen <69436315+contigen@users.noreply.github.com>
2023-09-08 09:30:24 +0100

    Comment out a config object field

--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {}
 
 const PWAConfig = require('next-pwa')({
   dest: `public`,
-  disable: isInDev,
+  // disable: isInDev,
   register: true,
   skipWaiting: true,
 })(nextConfig)
